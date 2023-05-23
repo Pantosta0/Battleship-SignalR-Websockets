759c608d26b65f1d4274fdb0b12ba082e0a59910
Santiago Palmet <santi1226@gmail.com>
2023-05-23 11:05:38 -0500

    Initial project files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Battleship_SignalR_Websockets.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Battleship_SignalR_Websockets.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
